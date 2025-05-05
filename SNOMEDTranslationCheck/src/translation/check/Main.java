0f7d5a6d850a1d8b21f56186ce69cb67dd207c89
Pero Grgic <grgicpero@hotmail.com>
2025-05-05 14:06:52 +0200

    Manipulation of list for DE imports fixed

--- SNOMEDTranslationCheck/src/translation/check/Main.java
+++ SNOMEDTranslationCheck/src/translation/check/Main.java
@@ -12,9 +12,9 @@ import java.sql.SQLException;
  */
 public class Main {
 
-	private static String CSVfilePath = "C:\\Users\\Pero Grgic\\Downloads\\Swiss_terms_20250403041047861.xlsx"; // Path to the CSV file with the terms to be compared
+	private static String CSVfilePath = "PATH_TO_YOUR_CSV/EXCEL_FILE"; // Path to the CSV/Excel file with the terms to be compared
 																	// with the SNOMED database
-	private static String destination = "C:\\Users\\Pero Grgic\\Desktop\\eHS\\SNOMED\\Swiss_Extension_Deltas"; // Path where to create the three files
+	private static String destination = "PATH_TO_YOUR_FOLDER"; // Path where to create the three files
 
 	public static long totalTime;
 
